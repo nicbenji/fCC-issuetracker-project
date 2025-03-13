75670de001929c26b6bd42dbb4b145177e63d70b
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-03-13 18:33:12 +0100

    added empty boilerplate for issueService

---
+++ services/issueService.js
@@ -0,0 +1,20 @@
+async function getAllIssues(filterOptions) {
+
+}
+
+async function createIssue(issue) {
+
+}
+
+async function updateIssueById(updateOptions) {
+
+}
+
+async function deleteIssueById(id) {
+
+}
+
+exports.getAllIssues = getAllIssues;
+exports.createIssue = createIssue;
+exports.updateIssueById = updateIssueById;
+exports.deleteIssueById = deleteIssueById;
