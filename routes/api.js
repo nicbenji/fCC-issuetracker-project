cd52d65b58817079a20bdaaf2cd9276704f5ece3
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-03-13 18:08:05 +0100

    added empty boilerplate and missing id checks in routes

--- routes/api.js
+++ routes/api.js
@@ -8,21 +8,45 @@ module.exports = function(app) {
       const project = req.params.project;
       const filters = req.query;
 
+      try {
+      } catch (error) {
+      }
+
     })
 
     .post(function(req, res) {
       const project = req.params.project;
+      const issue = req.body;
 
+      try {
+      } catch (error) {
+      }
     })
 
     .put(function(req, res) {
       const project = req.params.project;
+      const updateFields = req.body;
+      const id = updateFields._id;
 
+      if (!id) {
+        return res.json({ error: 'missing _id' });
+      }
+
+      try {
+      } catch (error) {
+      }
     })
 
     .delete(function(req, res) {
       const project = req.params.project;
+      const id = req.body._id;
+      if (!id) {
+        return res.json({ error: 'missing _id' });
+      }
 
+      try {
+      } catch (error) {
+      }
     });
 
 };
